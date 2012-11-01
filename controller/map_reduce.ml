c97c5bc1a25c2bc066a4efbbb0d432e20dc56620
Heming Ge <hwg33@cornell.edu>
2012-10-31 20:14:21 -0400

    MapReduce skeleton

--- controller/map_reduce.ml
+++ controller/map_reduce.ml
@@ -3,11 +3,25 @@ open Worker_manager
 
 (* TODO implement these *)
 let map kv_pairs shared_data map_filename : (string * string) list = 
-  failwith "Go back whence you came! Trouble the soul of my Mother no more!"
+  let q = initialize_mappers map_filename shared_data in
+  let tp = Thread_pool.create 50 in
+  let f () = failwith "" 
+
 let combine kv_pairs : (string * string list) list = 
-  failwith "You have been doomed ever since you lost the ability to love."
+  let t = Hashtable.create (List.length kv_pairs) Hashtbl.hash in
+  let helper t (k,v) = 
+    if Hashtable.mem t k then Hashtable.add t k (v::(Hashtable.find t k))
+    else Hashtable.add t k [v]; t in
+  let new_t = List.fold_left helper t kv_pairs in
+  let lst = ref [] in
+  Hashtable.iter (fun k v -> lst := (k,v)::(!lst)) new_t; !lst
+
 let reduce kvs_pairs shared_data reduce_filename : (string * string list) list =
-  failwith "The only thing necessary for evil to triumph is for good men to do nothing"
+  let q = initialize_reducers reduce_filename shared_data in
+  let helper acc (k,vs) = match reduce (pop_worker q) k vs with
+    | None -> failwith "Implement fault tolerance"
+    | Some lst -> (k,lst)::acc in
+  List.fold_left helper [] kvs_pairs
 
 let map_reduce (app_name : string) (mapper : string) 
     (reducer : string) (filename : string) =
