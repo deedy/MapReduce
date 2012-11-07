262b78912c174b9b121f659ca54f214831604f5f
Heming Ge <hwg33@cornell.edu>
2012-11-07 16:53:22 -0500

    Functional Map_reduce

--- apps/nbody/nbody.ml
+++ apps/nbody/nbody.ml
@@ -1,7 +1,7 @@
 open Util
 
 let map_reduce (app_name : string) (mapper : string) 
-    (reducer : string) (kv_pairs: (string*string) list) (shared_data: string)=
+    (reducer : string) (kv_pairs: (string * string) list) (shared_data: string)=
   let app_dir = Printf.sprintf "apps/%s/" app_name in
   let mapped = Map_reduce.map kv_pairs shared_data (app_dir ^ mapper ^ ".ml") in
   let combined = Map_reduce.combine mapped in
@@ -9,7 +9,7 @@ let map_reduce (app_name : string) (mapper : string)
   reduced
 
 
-let get_graph_array_from_bodies (bodies : (string*body) list) : body array = 
+let get_graph_array_from_bodies (bodies : (string * body) list) : body array = 
   let max_id = List.fold_left (fun acc (id,_) -> if ((int_of_string id) > acc) then 
     (int_of_string id) else acc) (-1) bodies in
   let bodies_array = Array.make (max_id+1) (-1.,(-1.,-1.),(-1.,-1.)) in
@@ -21,7 +21,7 @@ let get_kv_pairs_from_bodies (bodies: (string*body) list ) : (string*string) lis
   List.rev (List.fold_left (fun acc (id,_) -> (id,"")::acc) [] bodies)
 
 
-let get_bodies_from_results (results: (string*string list) list) (bodies_array: body array) : (string*body) list =
+let get_bodies_from_results (results: (string * string list) list) (bodies_array: body array) : (string * body) list =
     let foo acc (id,accel_list) = let iden = int_of_string id in 
       let (mass,(posx,posy),(velx,vely)) = Array.get bodies_array iden in
       let (accx,accy) = unmarshal (List.hd(accel_list)) in
@@ -42,7 +42,7 @@ let get_bodies_from_results (results: (string*string list) list) (bodies_array: 
 
     List.rev (List.fold_left foo [] results)
 
-let new_graph_array (bodies : (string*body) list) (bodies_array: body array) : body array = 
+let new_graph_array (bodies : (string * body) list) (bodies_array: body array) : body array = 
   let _ = List.fold_left (fun acc (id,bod) -> Array.set bodies_array 
          (int_of_string id) bod; acc) 0 bodies in 
   bodies_array
--- controller/map_reduce.ml
+++ controller/map_reduce.ml
@@ -1,54 +1,52 @@
 open Util
 open Worker_manager
 
-(* TODO implement these *)
 let map kv_pairs shared_data map_filename : (string * string) list = 
-  let manager = Worker_manager.initialize_mappers map_filename shared_data in
-  let thread_pool = Thread_pool.create 100 in
-  let work_map = Hashtable.create (List.length kv_pairs) Hashtbl.hash in
-  let _ = List.fold_left (fun acc ele -> Hashtable.add work_map acc ele;
-    (acc+1)) 0 kv_pairs in
-  let results_list = Hashtable.create (List.length kv_pairs) Hashtbl.hash in
-  let results_lock = Mutex.create() in
-  let work_lock = Mutex.create() in
-  let map_function (id,(key,value)) () =
-    let worker = Worker_manager.pop_worker manager in
-      match (Worker_manager.map worker key value) with
-      | Some (res) ->
-          let _ = Mutex.lock work_lock in
-          if (Hashtable.mem work_map id) then 
-            let _ = Hashtable.remove work_map id in
-            let _ = Mutex.unlock work_lock in
-            let _ = Mutex.lock results_lock in
-            let _ = Hashtable.add results_list id res in
-            let _ = Mutex.unlock results_lock in
-            Worker_manager.push_worker manager worker
-          else 
-            let _ = Mutex.unlock work_lock in
-            Worker_manager.push_worker manager worker
-      | None -> () (* Failed workers not pushed back onto queue *) 
-    in
-  let rec continualAdder work_map : (string * string) list = 
-    if Hashtable.length work_map = 0 then
-      let _ = Thread_pool.destroy thread_pool in
-      let _ = Worker_manager.clean_up_workers manager in
-      let _  = Mutex.lock results_lock in
-      let results = (Hashtable.fold (fun id value acc -> List.fold_left
-          (fun acc elm -> elm :: acc) acc value) results_list []) in
-      let _ = Mutex.unlock results_lock in
- (*     let _ = print_map_results results in*)
-      results
-    else
-      let _ = print_int  (Hashtable.length work_map) in
-      let _ = print_endline ("") in
-      let _ = Mutex.lock work_lock in
-      let _ = Hashtable.iter (fun id kv -> Thread_pool.add_work (map_function (id,kv)) thread_pool) work_map in
-      let _ = Mutex.unlock work_lock in 
-	    let _  = Thread.delay 0.1 in 
-      continualAdder work_map in
+  let num_pairs = List.length kv_pairs in
+  let manager = initialize_mappers map_filename shared_data in 
+  let t = Hashtable.create (List.length kv_pairs) Hashtbl.hash in
+  let i = ref 0 in
+  let helper t (k,v) = Hashtable.add t !i (k, v, ref 0); i := !i + 1; t in
+  let table = List.fold_left helper t kv_pairs in
+  let pool = Thread_pool.create 100 in
+  let resultLst = ref [] in 
+  let marker = ref 0 in
+  let tableLock = Mutex.create () in
+  let resultLock = Mutex.create () in
+  let rec f i () = 
+    Mutex.lock tableLock;
+    if Hashtable.mem table i then begin
+      let (k,v,c) = Hashtable.find table i in 
+      Mutex.unlock tableLock;
+      if !c > 5 then failwith "A job has failed" else
+      let worker = pop_worker manager in
+      match map worker k v with
+      | None -> c := !c + 1
+      | Some lst -> begin
+          Mutex.lock tableLock;
+          if Hashtable.mem table i then begin
+            Hashtable.remove table i; 
+            Mutex.unlock tableLock;
+            Mutex.lock resultLock; 
+            resultLst := lst::(!resultLst);
+            Mutex.unlock resultLock end
+          else Mutex.unlock tableLock; 
+          Mutex.lock tableLock;
+          if Hashtable.length table > 0 then begin
+            while Hashtable.mem table !marker = false do
+              marker := (!marker + 1) mod num_pairs done;
+            Thread_pool.add_work (f !marker) pool; 
+            marker := (!marker + 1) mod num_pairs end;
+          Mutex.unlock tableLock end;
+      push_worker manager worker end
+    else Mutex.unlock tableLock in
+  Hashtable.iter (fun k v -> Thread_pool.add_work (f k) pool) table;
+  while List.length !resultLst < num_pairs do Thread.delay 0.1 done;
+  Thread_pool.destroy pool;
+  clean_up_workers manager;
+  let flatten acc x = List.fold_left (fun a b -> b::a) acc (List.rev x) in
+  List.fold_left flatten [] (List.rev !resultLst)
 
-    continualAdder work_map
-  
 let combine kv_pairs : (string * string list) list = 
   let t = Hashtable.create (List.length kv_pairs) Hashtbl.hash in
   let helper t (k,v) = 
@@ -56,56 +54,52 @@ let combine kv_pairs : (string * string list) list =
     else Hashtable.add t k [v]; t in
   let new_t = List.fold_left helper t kv_pairs in
   let lst = ref [] in
-  Hashtable.iter (fun k v -> lst := (k,v)::(!lst)) new_t; 
-  let combined = !lst in
-  combined 
-
-
+  Hashtable.iter (fun k vs -> lst := (k,vs)::(!lst)) new_t; !lst
 
 let reduce kvs_pairs shared_data reduce_filename : (string * string list) list =
-  let manager = Worker_manager.initialize_reducers reduce_filename shared_data in
-  let thread_pool = Thread_pool.create 100 in
-  let work_map = Hashtable.create (List.length kvs_pairs) Hashtbl.hash in
-  let _ = List.fold_left (fun acc ele -> Hashtable.add work_map acc ele;
-    (acc+1)) 0 kvs_pairs in
-  let results_list = Hashtable.create (List.length kvs_pairs) Hashtbl.hash in
-  let results_lock = Mutex.create() in
-  let work_lock = Mutex.create() in
-  let reduce_function (id,(key,value)) () =
-    let worker = Worker_manager.pop_worker manager in
-      match (Worker_manager.reduce worker key value) with
-      | Some (res) ->
-          let _ = Mutex.lock work_lock in
-          if (Hashtable.mem work_map id) then 
-            let _ = Hashtable.remove work_map id in
-            let _ = Mutex.unlock work_lock in
-            let _ = Mutex.lock results_lock in
-            let _ = Hashtable.add results_list id (key,res) in
-            let _ = Mutex.unlock results_lock in
-            Worker_manager.push_worker manager worker
-          else 
-            let _ = Mutex.unlock work_lock in
-            Worker_manager.push_worker manager worker
-      | None -> () (* Failed workers not pushed back onto queue *) 
-    in
-  let rec continualAdder work_map : (string * string list) list = 
-    if Hashtable.length work_map = 0 then
-      let _ = Thread_pool.destroy thread_pool in
-      let _ = Worker_manager.clean_up_workers manager in
-      let _  = Mutex.lock results_lock in
-      let results = Hashtable.fold (fun id value acc -> value :: acc) 
-          results_list [] in
-      let _ = Mutex.unlock results_lock in
-      let _ = print_reduce_results results in
-      results
-    else
-      let _ = Mutex.lock work_lock in
-      let _ = Hashtable.iter (fun id kv -> Thread_pool.add_work (reduce_function (id,kv)) thread_pool) work_map in
-      let _ = Mutex.unlock work_lock in 
-      let _  = Thread.delay 0.1 in 
-      continualAdder work_map in
-  continualAdder work_map
-
+  let num_pairs = List.length kvs_pairs in
+  let manager = initialize_reducers reduce_filename shared_data in 
+  let t = Hashtable.create (List.length kvs_pairs) Hashtbl.hash in
+  let i = ref 0 in
+  let helper t (k,vs) = Hashtable.add t !i (k, vs, ref 0); i := !i + 1; t in
+  let table = List.fold_left helper t kvs_pairs in
+  let pool = Thread_pool.create 100 in
+  let resultLst = ref [] in 
+  let marker = ref 0 in
+  let tableLock = Mutex.create () in
+  let resultLock = Mutex.create () in
+  let rec f i () = 
+    Mutex.lock tableLock;
+    if Hashtable.mem table i then begin
+      let (k,vs,c) = Hashtable.find table i in 
+      Mutex.unlock tableLock;
+      if !c > 5 then failwith "A job has failed" else
+      let worker = pop_worker manager in
+      match reduce worker k vs with
+      | None -> c := !c + 1
+      | Some lst -> begin
+          Mutex.lock tableLock; 
+          if Hashtable.mem table i then begin
+            Hashtable.remove table i; 
+            Mutex.unlock tableLock;
+            Mutex.lock resultLock; 
+            resultLst := (k,lst)::(!resultLst); 
+            Mutex.unlock resultLock end
+          else Mutex.unlock tableLock; 
+          Mutex.lock tableLock;
+          if Hashtable.length table > 0 then begin
+            while Hashtable.mem table !marker = false do
+              marker := (!marker + 1) mod num_pairs done;
+            Thread_pool.add_work (f !marker) pool; 
+            marker := (!marker + 1) mod num_pairs end;
+          Mutex.unlock tableLock end;
+      push_worker manager worker end
+    else Mutex.unlock tableLock in
+  Hashtable.iter (fun k v -> Thread_pool.add_work (f k) pool) table;
+  while List.length !resultLst < num_pairs do Thread.delay 0.1 done;
+  Thread_pool.destroy pool;
+  clean_up_workers manager; 
+  !resultLst
 
 let map_reduce (app_name : string) (mapper : string) 
     (reducer : string) (filename : string) =
